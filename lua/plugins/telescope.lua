07e8e04f1466ee649dbf4f2ca2f4248876c3f02c
Jakub Walkowski <walkoskuba@gmail.com>
2025-12-23 15:33:10 +0100

    update treesitter and telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-media-files.nvim' },
 	config = function()
 		require('telescope').setup{
