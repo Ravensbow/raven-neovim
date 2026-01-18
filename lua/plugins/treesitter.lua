f2fe601259a4fc7ad0819aa2df64fed9ad1e1c20
Jakub Walkowski <walkoskuba@gmail.com>
2026-01-18 10:39:54 +0100

    Formatter with conform plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,18 +4,18 @@ return {
 	lazy = false,
 	build = ':TSUpdate',
 	config = function()
-		require'nvim-treesitter'.setup {
+		require 'nvim-treesitter'.setup {
 			-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
 			install_dir = vim.fn.stdpath('data') .. '/site'
 		}
 
-		vim.api.nvim_create_autocmd('FileType', 
-		{
-			pattern = { 'go', 'cs' },
-			callback = function ()
-				vim.treesitter.start()
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end
-		})
+		vim.api.nvim_create_autocmd('FileType',
+			{
+				pattern = { 'go', 'cs' },
+				callback = function()
+					vim.treesitter.start()
+					-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			})
 	end
 }
